4029e49cf7850247fe89dcb8c0538ec47db38162
simlenodir <nodirsmailov0@gmail.com>
2023-01-29 14:52:20 +0300

    added update validation

--- src/validation/validation.ts
+++ src/validation/validation.ts
@@ -1,7 +1,13 @@
 import Joi from "joi";
 
-export const UsersPost = Joi.object({
+export const UserPost = Joi.object({
     first_name: Joi.string().min(2).required(),
     last_name: Joi.string().min(2).required(),
     email: Joi.string().required()
+})
+
+export const UserUpdate = Joi.object({
+    first_name: Joi.string().min(2),
+    last_name: Joi.string().min(2),
+    email: Joi.string()
 })
